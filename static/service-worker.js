2fa82d4a564b5834454145704defa285fe597b6e
Dolores Derksen <doloresderksen@gmail.com>
2025-04-16 22:26:14 +0900

    Finished the PWA

--- static/service-worker.js
+++ static/service-worker.js
@@ -5,8 +5,8 @@ const urlsToCache = [
   "/",
   "/static/branding/Minimalist_Title.svg",
   "/static/manifest.json",
-  "/static/icons/icon-192.png",
-  "/static/icons/icon-512.png",
+  "/static/branding/android-chrome-192x192.png",
+  "/static/branding/android-chrome-512x512.png",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 ];
